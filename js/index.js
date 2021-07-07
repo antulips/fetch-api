2865b2ce7cfe29e34a44b37b0ba2feb0d96aa4b8
kushina <65832046+antulips@users.noreply.github.com>
2021-07-07 17:32:29 -0300

    [FIX] Exercise resolution - Team 4

--- js/index.js
+++ js/index.js
@@ -1,23 +1,34 @@
-// Aquí realizamos un la consulta de la promesa, esperando su respuesta asíncrona
-fetch('https://randomuser.me/api/')
-    .then(response => {
-        return response.json()
+window.onload = () => {
+    document.querySelector('#random').addEventListener('click', () => {
+        randomUser();
     })
-    .then(data => {
-        //manipulamos la respuesta
-        console.log(data)
-    });
+}
+
+// Aquí realizamos la consulta de la promesa, esperando su respuesta asíncrona
+function randomUser() {
+    fetch('https://randomuser.me/api/')
+        .then(response => {
+            return response.json()
+        })
+        .then( data => {
+            //manipulamos la respuesta
+            renderizarDatosUsuario(data.results[0])
+        });
+}
 
 function renderizarDatosUsuario(datos) {
     /* -------------------------------- CONSIGNA 1 -------------------------------- */
     // Aquí deben desarrollar una función que muestre en pantalla:
     // la foto, el nombre completo del usuario y su email.
     // Esto debe estar basado en la info que nos llega desde la API e insertarse en el HTML.
-    
-    
-    
-    
-    
+    const contenedor = document.querySelector('.tarjeta');
+
+    const template = `
+        <img src="${datos.picture.medium}" alt="Imagen de perfil.">
+        <p class="name"><span>Nombre:</span> ${datos.name.title}. ${datos.name.first} ${datos.name.last}</p>
+        <p class="email"><span>Email</span>: ${datos.email}</p>
+    `
+    contenedor.innerHTML = template;    
 }
 
 
